22e4bc9e1b6872ace997d85590aada15cb00b8f3
Velociraptor85 <hrspam@gmail.com>
2020-03-01 16:23:42 +0100

    typecasting that int

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ api = Api(app, errors=errors)
 
 app.config['MONGODB_DB'] = os.getenv('MONGODB_DATABASE', 'test')
 app.config['MONGODB_HOST'] = os.getenv('MONGODB_HOST', 'localhost')
-app.config['MONGODB_PORT'] = os.getenv(MONGODB_PORT, 27017)
+app.config['MONGODB_PORT'] = int(os.getenv('MONGODB_PORT', '27017'))
 app.config['MONGODB_USERNAME'] = os.getenv('MONGODB_USERNAME', '')
 app.config['MONGODB_PASSWORD'] = os.getenv('MONGODB_PASSWORD', '')
 #app.config['API_VERSION'] = 'v1.0'
