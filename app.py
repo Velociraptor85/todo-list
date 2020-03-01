d201a79fdbfbb495cd8579773b011a0f680ca921
Velociraptor85 <hrspam@gmail.com>
2020-03-01 17:21:03 +0100

    testgin

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from resources.errors import errors
 app = Flask(__name__)
 api = Api(app, errors=errors)
 
-app.config['MONGODB_DB'] = os.getenv('MONGODB_DATABASE', 'test')
+app.config['MONGODB_DB'] = os.getenv('MONGODB_DATABASE', 'sampledb')
 app.config['MONGODB_HOST'] = os.getenv('MONGODB_HOST', 'localhost')
-app.config['MONGODB_PORT'] = int(os.getenv('MONGODB_PORT', '27017'))
+app.config['MONGODB_PORT'] = os.getenv('MONGODB_PORT', 27017)
 app.config['MONGODB_USERNAME'] = os.getenv('MONGODB_USERNAME', '')
 app.config['MONGODB_PASSWORD'] = os.getenv('MONGODB_PASSWORD', '')
 #app.config['API_VERSION'] = 'v1.0'
