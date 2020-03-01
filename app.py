e21313cd6f3436039336a40d7362e77563594c36
Velociraptor85 <hrspam@gmail.com>
2020-03-01 17:29:06 +0100

    fixing through git ^^

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ api = Api(app, errors=errors)
 
 app.config['MONGODB_DB'] = os.getenv('MONGODB_DATABASE', 'sampledb')
 app.config['MONGODB_HOST'] = os.getenv('MONGODB_HOST', 'localhost')
-app.config['MONGODB_PORT'] = os.getenv('MONGODB_PORT', 27017)
+app.config['MONGODB_PORT'] = int(os.getenv('MONGODB_PORT', '27017'))
 app.config['MONGODB_USERNAME'] = os.getenv('MONGODB_USERNAME', '')
 app.config['MONGODB_PASSWORD'] = os.getenv('MONGODB_PASSWORD', '')
 #app.config['API_VERSION'] = 'v1.0'
+
 
 #initialize Database
 initialize_db(app)
