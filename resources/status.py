79946230bd8d2ee0a2ca2c232de794832abccf81
Velociraptor85 <hrspam@gmail.com>
2020-03-07 18:13:02 +0100

    need correct jsons here

--- resources/status.py
+++ resources/status.py
@@ -6,28 +6,28 @@ from resources.errors import InternalServerError
 class Hostname(Resource):
     def get(self):
         try:
-            return Response(socket.gethostname(), mimetype="application/json", status=200)
+            return Response('{"hostname": "' + str(socket.gethostname()) + '"}', mimetype="application/json", status=200)
         except Exception:
             raise InternalServerError
 
 class APIVersion(Resource):
     def get(self):
         try:
-            return Response('v1.0', mimetype="application/json", status=200)
+            return Response('{"api_version": "v1.0"}', mimetype="application/json", status=200)
         except Exception:
             raise InternalServerError
 
 class Uptime(Resource):
     def get(self):
         try:
-            return Response('', mimetype="application/json", status=200)
+            return Response('{"uptime": ""}', mimetype="application/json", status=200)
         except Exception:
             raise InternalServerError
 
 class Running(Resource):
     def get(self):
         try:
-            return Response('True', mimetype="application/json", status=200)
+            return Response('{"running": true}', mimetype="application/json", status=200)
         except Exception:
             raise InternalServerError
 
